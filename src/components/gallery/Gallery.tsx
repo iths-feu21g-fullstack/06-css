1c9e3035e7f5df697027ca20991a869994dee152
David Andersson <lejonmanen@gmail.com>
2022-04-29 10:55:00 +0200

    Gallery with images

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,8 +1,18 @@
-
+import fish1 from './fish1.jpg'
+import fish2 from './fish2.jpg'
+import fish3 from './fish3.jpg'
+import fish4 from './fish4.jpg'
+import './Gallery.css'
 
 const Gallery = () => (
-	<section>
+	<section className="gallery">
 		Gallery
+		<div className="img-container">
+			<img src={fish1} />
+			<img src={fish2} />
+			<img src={fish3} />
+			<img src={fish4} />
+		</div>
 	</section>
 )
 
